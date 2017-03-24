c890368317fc69ede3df628303b7a13a796175a0
Bernd Bischl <bernd_bischl@gmx.net>
2017-03-24 16:47:23 +0100

    cocoBenchmarkOptimizer

---
+++ R/cocoBenchmarkOptimizer.R
@@ -0,0 +1,25 @@
+#' @title Run optimizer on complete coco suite.
+#' 
+#' @description
+#' The function is mainly called for its side effect of coco logging of results on disk.
+#' It is expected that the caller opens the suite with \code{\link{cocoOpenSuite}} and closes the 
+#' suite with \code{\link{cocoCloseSuite}}.
+#'
+#' @template arg_optimizer
+#' @template arg_suite
+#' @param \ldots [any]\cr
+#'   Passed down to \code{optimizer}.
+#' @return [\code{list}]. List of results for individual \code{\link{cocoRunOptimizer}} calls.
+#'   List is named with coco problem ids.
+#' @export
+cocoBenchmarkOptimizer = function(optimizer, suite, ...) {
+  assertFunction(optimizer, c("fn", "start", "problem"))
+  assertClass(suite, "CocoSuite")
+  res = list()
+  while(!is.null(problem <- cocoSuiteGetNextProblem(suite))) {
+    r = cocoRunOptimizer(optimizer, problem, ...)
+    res[[problem$id]] = r
+  }
+  return(res)
+}
+
