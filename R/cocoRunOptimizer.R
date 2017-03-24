257870c8fbb2c14672feeaf3fc011269a44a83e9
Bernd Bischl <bernd_bischl@gmx.net>
2017-03-24 14:20:42 +0100

    improve cocoRunOptimizer

--- R/cocoRunOptimizer.R
+++ R/cocoRunOptimizer.R
@@ -1,9 +1,18 @@
-cocoRunOptimizer = function(optfun, problem, ...) {
-  assertFunction(optfun)
+#' @title Run optimizer on specific coco problem.
+#' 
+#' @description
+#' The function is mainly called for its side effect of coco logging of results on disk.
+#'
+#' @template arg_optimizer
+#' @template arg_problem
+#' @return Result object that \code{optimizer} returns.
+#' @export
+cocoRunOptimizer = function(optimizer, problem, ...) {
+  assertFunction(optimizer, c("fn", "start", "problem"))
   assertClass(problem, "CocoProblem")
   fn = function(x) {
     cocoEvaluateFunction(problem, x)
   }
   start = cocoProblemGetInitialSolution(problem)
-  optfun(fn, start, problem, ...)
+  optimizer(fn, start, problem, ...)
 }
