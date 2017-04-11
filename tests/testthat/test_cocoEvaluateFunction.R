829d2292fa2eabe37327080ea2bdccce189b26dd
Jakob Bossek <info@jakobbossek.de>
2017-04-11 21:59:22 +0200

    fix tests

--- tests/testthat/test_cocoEvaluateFunction.R
+++ tests/testthat/test_cocoEvaluateFunction.R
@@ -2,10 +2,11 @@ context("cocoEvaluateFunction")
 
 test_that("cocoEvaluateFunction", {
   s = cocoOpenSuite()
-  p = cocoSuiteGetNextProblem(s)
+  o = cocoInitObserver("bbob")
+  p = cocoSuiteGetNextProblem(s, o)
   expect_equal(cocoProblemGetEvaluations(p), 0L)
   x = c(0, 0)
-  y = cocoEvaluateFunction(p, x) 
+  y = cocoEvaluateFunction(p, x)
   expect_is(y, "numeric")
   expect_length(y, 1L)
   expect_equal(cocoProblemGetEvaluations(p), 1L)
--- tests/testthat/test_cocoSuiteGetProblem.R
+++ tests/testthat/test_cocoSuiteGetProblem.R
@@ -2,19 +2,20 @@ context("cocoSuiteGetProblem")
 
 test_that("cocoSuiteGetProblem", {
   s = cocoOpenSuite()
+  o = cocoInitObserver()
   # iterate normal noiseless bbob, 6 dims (d = 2,5,10,20,40) x 24 funs x 15 function instances
   for (i in 1:(6 * 24 * 15)) {
-    p1 = cocoSuiteGetNextProblem(s)
+    p1 = cocoSuiteGetNextProblem(s, o)
     p2 = cocoSuiteGetProblem(s, i-1)
     expect_output(print(p1), "CocoProblem", info = i)
     expect_equal(p1$index, i-1)
     # FIXME: it is weird that the problems return ONLY differen names? looks like a bug in coco?
-    # i openend an issue about this 
+    # i openend an issue about this
     p1$name = p2$name
     expect_equal(p1, p2)
   }
   # last call should return NULL
-  p = cocoSuiteGetNextProblem(s)
+  p = cocoSuiteGetNextProblem(s, o)
   expect_null(p)
   cocoCloseSuite(s)
 })
